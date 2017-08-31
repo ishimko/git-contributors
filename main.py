e04c9afd15a524ae28c465856bc661a76e096a4e
vanashimko <vana.shimko@gmail.com>
2017-08-31 17:07:50 +0300

    Add short alias and help for --include-zeros parameter

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from git import Git
 def main():
     parser = ArgumentParser()
     parser.add_argument('path', nargs='?')
-    parser.add_argument('--include-zeros', action='store_true')
+    parser.add_argument('-z', '--include-zeros', action='store_true',
+                        help="include contributors with zero contributions")
     args = parser.parse_args()
     git = Git(args.path)
     for user, statistics in git.get_contribution_statistics(args.include_zeros):
